a05ec7d0f3a4693feae890d6f6314e09c0ab97af
Mariela Belaunde <mariela.belaunde@gmail.com>
2022-04-14 12:17:24 -0400

    Some seeds, routes and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating 5 products...'
+5.times do |i|
+  article = Article.create!(
+    title: Faker::Book.title,
+    content: Faker::Quote.matz
+  )
+  puts "#{i + 1}. #{article.title}"
+end
+puts 'Finished!'
